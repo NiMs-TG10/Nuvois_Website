2b131aeb41be58f2d788d62e24966b690843928e
NiMs-TG10 <mehtanishant1030@gmail.com>
2025-11-15 12:57:11 +0530

    FEAT:Title and discription

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Nuvois.ai",
+  description: "Your Style. Perfected by AI.",
+  generator: "Nishant",
 };
 
 export default function RootLayout({
